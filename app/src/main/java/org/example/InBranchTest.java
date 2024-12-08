059967b76b734094e276d3ad3a2c9e4bea1cf879
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2024-12-08 19:47:57 +0100

    Set to fail in main

---
+++ app/src/main/java/org/example/InBranchTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Useless class
+ */
+public class InBranchTest {
+    public boolean fail() {
+        return false;
+    }
+
+    public boolean succeed() {
+        return true;
+    } 
+}
